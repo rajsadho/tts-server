01b6b9ed6b25049fb64c8ce318d2e833d287ef29
rajsadho <rajsadho@users.noreply.github.com>
2022-04-22 07:51:16 -0400

    Cleanup.

--- Dockerfile
+++ Dockerfile
@@ -2,25 +2,6 @@ FROM rajivsadho/pytorch-tts
 
 WORKDIR /app
 
-ARG use_cuda=False
-ENV use_cuda=${use_cuda}
-
-ARG config_path=False
-ENV config_path=${config_path}
-
-ARG model_path=False
-ENV model_path=${model_path}
-
-ARG vocoder_config_path=False
-ENV vocoder_config_path=${vocoder_config_path}
-
-ARG vocoder_path=False
-ENV vocoder_path=${vocoder_path}
-
-ARG debug=False
-ENV debug=${debug}
-
-
 COPY . .
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-#!flask/bin/python
-import argparse
 import io
 import json
 import os
-import sys
-from pathlib import Path
 from typing import Union
 
 from flask import Flask, render_template, request, send_file
@@ -14,68 +10,32 @@ from TTS.utils.synthesizer import Synthesizer
 
 app = Flask(__name__)
 
-# app.config["model_path"] = os.environ.get("model_path")
-# app.config["config_path"] = os.environ.get("config_path")
-# app.config["speakers_file_path"] = os.environ.get("speakers_file_path", None)
-# app.config["vocoder_path"] = os.environ.get("vocoder_path")
-# app.config["vocoder_config_path"] = os.environ.get("vocoder_config_path")
-# app.config["port"] = os.environ.get("port", 5002)
-# app.config["use_cuda"] = os.environ.get("use_cuda", False)
-# app.config["debug"] = os.environ.get("debug", False)
-# app.config["show_details"] = os.environ.get("show_details", False)
-
-# parse the args
-# args = app.config.__dict__
-
-
-# update in-use models to the specified released models.
-# model_path = None
-# config_path = None
-# speakers_file_path = None
-# vocoder_path = None
-# vocoder_config_path = None
-# port = 5002
-# use_cuda = False
-# debug = False
-# show_details = False
-
-model_path="/app/model_data/models/female.pth"
-config_path="/app/model_data/models/config.json"
-speakers_file_path=None
-vocoder_path="/app/model_data/vocoders/model_file.pth.tar"
-vocoder_config_path="/app/model_data/vocoders/config.json"
-port=5002
-use_cuda=False
-debug=True
-show_details=True
-
-# model_path = os.environ.get("model_path", "/app/model_data/models/female.pth")
-# config_path = os.environ.get("config_path", "/app/model_data/models/config.json")
-# # speakers_file_path = os.environ.get("speakers_file_path", None)
-# vocoder_path = os.environ.get("vocoder_path", "/app/model_data/vocoders/model_file.pth.tar")
-# vocoder_config_path = os.environ.get("vocoder_config_path", "/app/model_data/vocoders/config.json")
-# port = os.environ.get("port", 5002)
-# use_cuda = False
-# debug = os.environ.get("debug", False)
-# show_details = os.environ.get("show_details", False)
+# Hardcoded config. Host does not play well with env.
+model_path = "/app/model_data/models/female.pth"
+config_path = "/app/model_data/models/config.json"
+speakers_file_path = None
+vocoder_path = "/app/model_data/vocoders/model_file.pth.tar"
+vocoder_config_path = "/app/model_data/vocoders/config.json"
+port = 5002
+use_cuda = False
+debug = True
+show_details = True
 
 args = {
-"model_path": model_path,
-"config_path": config_path,
-# "speakers_file_path": os.environ.get("speakers_file_path", None),
-"vocoder_path": vocoder_path,
-"vocoder_config_path": vocoder_config_path,
-"port": port,
-"use_cuda": False,
-"debug": debug,
-"show_details": show_details
+    "model_path": model_path,
+    "config_path": config_path,
+    "vocoder_path": vocoder_path,
+    "vocoder_config_path": vocoder_config_path,
+    "port": port,
+    "use_cuda": False,
+    "debug": debug,
+    "show_details": show_details,
 }
 
 # load models
 synthesizer = Synthesizer(
     tts_checkpoint=model_path,
     tts_config_path=config_path,
-    # tts_speakers_file=speakers_file_path,
     vocoder_checkpoint=vocoder_path,
     vocoder_config=vocoder_config_path,
     encoder_checkpoint="",
@@ -83,13 +43,14 @@ synthesizer = Synthesizer(
     use_cuda=use_cuda,
 )
 
-print(args)
 
-use_multi_speaker = hasattr(synthesizer.tts_model, "num_speakers") and synthesizer.tts_model.num_speakers > 1
+use_multi_speaker = (
+    hasattr(synthesizer.tts_model, "num_speakers")
+    and synthesizer.tts_model.num_speakers > 1
+)
 speaker_manager = getattr(synthesizer.tts_model, "speaker_manager", None)
 # TODO: set this from SpeakerManager
 use_gst = synthesizer.tts_config.get("use_gst", False)
-
 
 
 def style_wav_uri_to_dict(style_wav: str) -> Union[str, dict]:
@@ -117,7 +78,9 @@ def index():
         "index.html",
         show_details=show_details,
         use_multi_speaker=use_multi_speaker,
-        speaker_ids=speaker_manager.speaker_ids if speaker_manager is not None else None,
+        speaker_ids=speaker_manager.speaker_ids
+        if speaker_manager is not None
+        else None,
         use_gst=use_gst,
     )
 
@@ -151,7 +114,6 @@ def tts():
     out = io.BytesIO()
     synthesizer.save_wav(wavs, out)
     return send_file(out, mimetype="audio/wav")
-
 
 
 def main():
